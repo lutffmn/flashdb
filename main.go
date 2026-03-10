bf79b2b012cc68cf144bc269ba2f2e8ae2c654b5
lutffmn <lutffmn@gmail.com>
2026-03-10 13:40:42 +0700

    feat: add logger

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ type FlashDB struct {
 	mu    sync.RWMutex
 	store map[string]DataItem
 
+	eventChan chan string
+
 	statsMu     sync.Mutex
 	totalReads  int
 	totalWrites int
@@ -29,6 +31,7 @@ func (f *FlashDB) Set(key string, value interface{}) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 	f.store[key] = DataItem{value}
+	f.eventChan <- fmt.Sprintf("EVENT SET KEY: %s", key)
 
 	f.statsMu.Lock()
 	defer f.statsMu.Unlock()
@@ -52,6 +55,7 @@ func (f *FlashDB) Delete(key string) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 	delete(f.store, key)
+	f.eventChan <- fmt.Sprintf("EVENT DELETE KEY: %s", key)
 }
 
 func (f *FlashDB) GetStats() (int, int) {
@@ -59,10 +63,21 @@ func (f *FlashDB) GetStats() (int, int) {
 }
 
 func main() {
+	pipeCh := make(chan string, 100)
+	stateCh := make(chan bool)
 	var wg sync.WaitGroup
 	db := &FlashDB{
-		store: make(map[string]DataItem),
+		store:     make(map[string]DataItem),
+		eventChan: pipeCh,
 	}
+
+	go func() {
+		for event := range pipeCh {
+			fmt.Println("Received event:", event)
+		}
+
+		stateCh <- true
+	}()
 
 	for range 100 {
 		wg.Go(func() {
@@ -77,9 +92,14 @@ func main() {
 			db.Get("robert")
 		})
 	}
+
 	wg.Wait()
 	fmt.Println(db.totalReads)
 
 	db.Delete("robert")
 	fmt.Println(db.GetStats())
+	close(pipeCh)
+	<-stateCh
+	close(stateCh)
+	fmt.Println("Exiting main")
 }
